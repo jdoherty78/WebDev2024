70a9aa72f0c42048960a90bca10e4d665e5cd9e0
jdoherty78 <joedoherty78@gmail.com>
2024-03-18 10:18:43 -0400

    eventListener keydown

--- JS/DOM/Drum-Kit/index.js
+++ JS/DOM/Drum-Kit/index.js
@@ -1,3 +1,5 @@
+// Detecting button press
+
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 // show alert when any button clicked with css class .drum
@@ -23,47 +25,71 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
         //this.style.color = "white";
         var buttonInnerHTML = this.innerHTML;    
-        switch (buttonInnerHTML) {
 
-            case "w":
-                var tom1 = new Audio("sounds/tom-1.mp3");    
-                tom1.play();
-              break;
+        makeSound(buttonInnerHTML);  // for the keyboard event
 
-              case "a":
-                var tom2 = new Audio("sounds/tom-2.mp3");    
-                tom2.play();
-              break;                
-
-              case "s":
-                var tom3 = new Audio("sounds/tom-3.mp3");    
-                tom3.play();
-              break;                   
-
-              case "d":
-                var tom4 = new Audio("sounds/tom-4.mp3");    
-                tom4.play();
-              break; 
-
-              case "j":
-                var snare = new Audio("sounds/snare.mp3");    
-                snare.play();
-              break; 
-
-              case "k":
-                var crash = new Audio("sounds/crash.mp3");    
-                crash.play();
-              break; 
-
-              case "l":
-                var kick = new Audio("sounds/kick-bass.mp3");    
-                kick.play();
-              break;               
-
-
-            default: console.log(buttonInnerHTML);
-                break;
-        }
+        
 
     });
 }    
+
+
+// Detecting keyboard press
+
+// keyboard response
+
+//addEventListener("keydown", function() {alert("Key was pressed!");});
+
+
+// let's find out what "event" is happening 
+// it will console.log the keyboard event that triggers the eventListener
+document.addEventListener("keydown", function(event){
+  //console.log(event);
+  makeSound(event.key);
+});
+
+function makeSound(key) {
+    switch (key) {
+
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");    
+            tom1.play();
+          break;
+
+          case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");    
+            tom2.play();
+          break;                
+
+          case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");    
+            tom3.play();
+          break;                   
+
+          case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");    
+            tom4.play();
+          break; 
+
+          case "j":
+            var snare = new Audio("sounds/snare.mp3");    
+            snare.play();
+          break; 
+
+          case "k":
+            var crash = new Audio("sounds/crash.mp3");    
+            crash.play();
+          break; 
+
+          case "l":
+            var kick = new Audio("sounds/kick-bass.mp3");    
+            kick.play();
+          break;               
+
+
+        default: console.log(buttonInnerHTML);
+            break;
+    }
+
+}
+
